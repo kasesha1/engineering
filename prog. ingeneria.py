eafdae8bcf97e93ae80bd9d83372efc7ead18068
kasesha1 <153167184+kasesha1@users.noreply.github.com>
2023-12-07 11:30:34 +0300

    Delete prog. ingeneria.py

--- prog. ingeneria.py
+++
@@ -1,9 +0,0 @@
-import math
-def x(z):
-    return pow(z, 3)-1
-def y():
-    return math.cos(5)*math.cos(5)
-def func(x, y):
-    print("Otvet:", x + y)
-z = int(input("введите переменную z:"))
-func(x(z), y())
