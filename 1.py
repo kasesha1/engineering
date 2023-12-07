e61b670d50d852481b8d46d6984897a106cd44b2
kasesha1 <153167184+kasesha1@users.noreply.github.com>
2023-12-07 11:28:04 +0300

    1.py

--- 1.py
+++ 1.py
@@ -1,9 +1,12 @@
 import math
-z = int(input("Введите z: "))
-y = lambda: math.cos(5) * math.cos(5)
-x = lambda z: pow(z, 3)-1
-func = lambda x, y: x + y
-print("otvet:", func(x(z), y()))
+def x(z):
+    return lambda: pow(z, 3)-1
+def y():
+    return lambda: math.cos(5)*math.cos(5)
+def func(x, y):
+    print("Otvet:", x() + y())
+z = int(input("введите переменную z:"))
+func(x(z), y())
 
 
 
